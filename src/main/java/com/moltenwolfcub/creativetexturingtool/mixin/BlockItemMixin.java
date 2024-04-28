3db6f9db4c993d7085797da8802ba1c357c45360
MoltenWolfCub <oliver@tomlins.org.uk>
2024-04-28 21:46:12 +0100

    Blocked normal placing
    while paintbrush is active blocks can't be placed normally

---
+++ src/main/java/com/moltenwolfcub/creativetexturingtool/mixin/BlockItemMixin.java
@@ -0,0 +1,32 @@
+package com.moltenwolfcub.creativetexturingtool.mixin;
+
+import com.moltenwolfcub.creativetexturingtool.DataSaver;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BlockItem.class)
+public abstract class BlockItemMixin extends Item {
+    public BlockItemMixin(Properties properties) {
+        super(properties);
+    }
+
+    @Inject(method = "place", at = @At("HEAD"), cancellable = true)
+    private void placeInjector(BlockPlaceContext blockPlaceContext, CallbackInfoReturnable<InteractionResult> info) {
+        Player player = blockPlaceContext.getPlayer();
+        Level level = blockPlaceContext.getLevel();
+        InteractionHand hand = blockPlaceContext.getHand();
+
+        if(((DataSaver)player).getPersistantData().getBoolean("active")){
+            info.setReturnValue(this.use(level, player, hand).getResult());
+        }
+    }
+}
