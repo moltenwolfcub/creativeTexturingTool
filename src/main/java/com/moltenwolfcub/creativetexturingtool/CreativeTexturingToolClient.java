9e68d535e7b29cd406fbfc40eb79e06c01bd3be2
MoltenWolfCub <oliver@tomlins.org.uk>
2024-05-28 16:05:30 +0100

    Added Size changing keybindings

--- src/main/java/com/moltenwolfcub/creativetexturingtool/CreativeTexturingToolClient.java
+++ src/main/java/com/moltenwolfcub/creativetexturingtool/CreativeTexturingToolClient.java
@@ -5,13 +5,18 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import org.lwjgl.glfw.GLFW;
 
+import java.util.function.Consumer;
+
 public class CreativeTexturingToolClient implements ClientModInitializer {
     private static final KeyMapping toggle;
+    private static final KeyMapping increaseSize;
+    private static final KeyMapping decreaseSize;
 
     static {
         toggle = KeyBindingHelper.registerKeyBinding(new KeyMapping(
@@ -20,6 +25,31 @@ public class CreativeTexturingToolClient implements ClientModInitializer {
                 GLFW.GLFW_KEY_APOSTROPHE,
                 "category."+CreativeTexturingTool.MODID
         ));
+        increaseSize = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+                "key."+CreativeTexturingTool.MODID+ ".increaseSize",
+                InputConstants.Type.KEYSYM,
+                GLFW.GLFW_KEY_KP_ADD,
+                "category."+CreativeTexturingTool.MODID
+        ));
+        decreaseSize = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+                "key."+CreativeTexturingTool.MODID+ ".decreaseSize",
+                InputConstants.Type.KEYSYM,
+                GLFW.GLFW_KEY_KP_SUBTRACT,
+                "category."+CreativeTexturingTool.MODID
+        ));
+    }
+
+    private void handleKeybind(KeyMapping key, Consumer<ServerPlayer> behaviour, Minecraft client){
+        while (key.consumeClick()) {
+            if (client.hasSingleplayerServer()){
+                MinecraftServer server = client.getSingleplayerServer();
+                ServerPlayer player = server.getPlayerList().getPlayer(client.player.getUUID());
+                behaviour.accept(player);
+
+            } else {
+                client.player.sendSystemMessage(Component.literal("Can't use keybinds in server setting."));
+            }
+        }
     }
 
     @Override
@@ -35,6 +65,26 @@ public class CreativeTexturingToolClient implements ClientModInitializer {
                     client.player.sendSystemMessage(Component.literal("Can't use keybinds in server setting."));
                 }
             }
+            while (increaseSize.consumeClick()) {
+                if (client.hasSingleplayerServer()){
+                    MinecraftServer server = client.getSingleplayerServer();
+                    ServerPlayer player = server.getPlayerList().getPlayer(client.player.getUUID());
+                    increaseSize(player);
+
+                } else {
+                    client.player.sendSystemMessage(Component.literal("Can't use keybinds in server setting."));
+                }
+            }
+            while (decreaseSize.consumeClick()) {
+                if (client.hasSingleplayerServer()){
+                    MinecraftServer server = client.getSingleplayerServer();
+                    ServerPlayer player = server.getPlayerList().getPlayer(client.player.getUUID());
+                    decreaseSize(player);
+
+                } else {
+                    client.player.sendSystemMessage(Component.literal("Can't use keybinds in server setting."));
+                }
+            }
         });
     }
 
@@ -44,5 +94,29 @@ public class CreativeTexturingToolClient implements ClientModInitializer {
 
         dataSaver.getPersistantData().putBoolean("active", !current);
         player.sendSystemMessage(Component.literal("Toggled paintbrush active to "+!current));
+    }
+    private static void increaseSize(ServerPlayer player) {
+        DataSaver dataSaver = (DataSaver) player;
+        int current = dataSaver.getPersistantData().getInt("size");
+        int newSize = current+1;
+
+        dataSaver.getPersistantData().putInt("size", newSize);
+        player.sendSystemMessage(Component.literal("Increased paintbrush size ("+newSize+")"));
+    }
+    private static void decreaseSize(ServerPlayer player) {
+        DataSaver dataSaver = (DataSaver) player;
+        int current = dataSaver.getPersistantData().getInt("size");
+        if (current < 1){
+            dataSaver.getPersistantData().putInt("size", 1);
+        }
+        if (current == 1){
+            player.sendSystemMessage(Component.literal("Can't make size any smaller than "+current));
+            return;
+        }
+
+        int newSize = current-1;
+
+        dataSaver.getPersistantData().putInt("size", newSize);
+        player.sendSystemMessage(Component.literal("Decreased paintbrush size ("+newSize+")"));
     }
 }
